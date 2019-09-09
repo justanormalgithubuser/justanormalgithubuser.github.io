84d988495265e68037b81a37ede66bd57125be3e
justanormalgithubuser <55021442+justanormalgithubuser@users.noreply.github.com>
2019-09-08 22:38:27 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 let elam = document.createElement('div');
-elam.style.csstext = 'color:' + '\u0119' + '\u0104' + '\u0105' + '\u0116' + '\u0101';
+elam.style.csstext = 'color:black";
 elam.appendChild('hint later');
 document.body.appendChild(elam);
